5fb97719e4799bd4e07c794472b706eff4e32d1e
manupadillaph <manuelpad@gmail.com>
2023-06-05 20:45:20 -0500

    docker

--- .appcontainer/test.sh
+++ .appcontainer/test.sh
@@ -1,2 +1,3 @@
 
-docker run -d -it --name rats-conteiner -p 3000:3000 -p 3080:3080  --env-file .env.local.copy manupadilla/rats-staking-git:7fbbf6c694accae271cf5c3c94703195fae7132b bash ./start-server.sh
+docker run -d -it --name rats-conteiner -p 3000:3000 -p 3080:3080  manupadilla/rats-staking-git:ea27ca419ee82341b0549b5c6590ad2d665198d3 bash ./start-server.sh
+ --env-file .env.local.copy 
