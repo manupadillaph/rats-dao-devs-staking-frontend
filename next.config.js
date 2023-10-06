5abb314ff6c40d6933eafff5cb0b603722ced278
manupadillaph <manuepad@gmail.com>
2023-10-05 18:12:56 -0600

    add  'mayz.io' to images domains

--- next.config.js
+++ next.config.js
@@ -1,71 +1,75 @@
 /** @type {import('next').NextConfig} */
-const {join} = require('path')
-const {access, symlink} = require('fs/promises')
+const { join } = require('path')
+const { access, symlink } = require('fs/promises')
 module.exports = {
   reactStrictMode: false,
-  images : {
-    domains: ['ratsdao.io','upload.wikimedia.org','upload.wikimedia.org','**.amazonaws.com','aldea-nft-logo.s3.sa-east-1.amazonaws.com','falcon-logo.s3.sa-east-1.amazonaws.com','https://falcon-logo.s3.sa-east-1.amazonaws.com/']
+  images: {
+    domains: [
+      'ratsdao.io',
+      'upload.wikimedia.org',
+      'upload.wikimedia.org',
+      '**.amazonaws.com',
+      'aldea-nft-logo.s3.sa-east-1.amazonaws.com',
+      'falcon-logo.s3.sa-east-1.amazonaws.com',
+      'mayz.io',
+    ],
   },
-  webpack: (config, { isServer }) =>  
-  // {
-  //   if (isServer) {
-  //     config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
-  //   } else {
-  //     config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
-  //   }
-  //   config.experiments = { 
-  //     asyncWebAssembly: true, 
-  //     topLevelAwait: true, 
-  //     layers: true 
-  //   };
-  //   config.optimization.moduleIds = 'named';
-  //   return config;
-  // },
-  
-  {
-    config.experiments = { 
-      asyncWebAssembly: true,
-      topLevelAwait: true,
-      layers: true
-    }
+  //'https://falcon-logo.s3.sa-east-1.amazonaws.com/'
+  webpack: (config, { isServer }) =>
+    // {
+    //   if (isServer) {
+    //     config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    //   } else {
+    //     config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    //   }
+    //   config.experiments = {
+    //     asyncWebAssembly: true,
+    //     topLevelAwait: true,
+    //     layers: true
+    //   };
+    //   config.optimization.moduleIds = 'named';
+    //   return config;
+    // },
+    {
+      config.experiments = {
+        asyncWebAssembly: true,
+        topLevelAwait: true,
+        layers: true,
+      }
 
-    config.plugins.push(
-      new (class {
-        apply(compiler) {
-          compiler.hooks.afterEmit.tapPromise(
-            'SymlinkWebpackPlugin',
-            async (compiler) => {
-              if (isServer) {
-                const from = join(compiler.options.output.path, '../static');
-                const to = join(compiler.options.output.path, 'static');
-    
-                try {
-                  await access(from);
-                  // console.log(`${from} already exists`);
-                  return;
-                } catch (error) {
-                  if (error.code === 'ENOENT') {
-                    // No link exists
-                  } else {
-                    throw error;
+      config.plugins.push(
+        new (class {
+          apply(compiler) {
+            compiler.hooks.afterEmit.tapPromise(
+              'SymlinkWebpackPlugin',
+              async (compiler) => {
+                if (isServer) {
+                  const from = join(compiler.options.output.path, '../static')
+                  const to = join(compiler.options.output.path, 'static')
+
+                  try {
+                    await access(from)
+                    // console.log(`${from} already exists`);
+                    return
+                  } catch (error) {
+                    if (error.code === 'ENOENT') {
+                      // No link exists
+                    } else {
+                      throw error
+                    }
                   }
+
+                  await symlink(to, from, 'junction')
+                  console.log(`created symlink ${from} -> ${to}`)
                 }
-    
-                await symlink(to, from, 'junction');
-                console.log(`created symlink ${from} -> ${to}`);
               }
-            },
-          );
-        }
-      })(),
-    );
-    return config
-  }
+            )
+          }
+        })()
+      )
+      return config
+    },
 }
-
-
-
-
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
@@ -76,10 +80,10 @@ module.exports = {
 //     } else {
 //       config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
 //     }
-//     config.experiments = { 
-//       asyncWebAssembly: true, 
-//       topLevelAwait: true, 
-//       layers: true  
+//     config.experiments = {
+//       asyncWebAssembly: true,
+//       topLevelAwait: true,
+//       layers: true
 //     };
 //     config.optimization.moduleIds = 'named';
 
@@ -88,4 +92,3 @@ module.exports = {
 // };
 
 // module.exports = nextConfig;
-
